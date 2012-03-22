15de0cc7c6d9857a05b739deaec565996c5f4a8b
Irfan Durmus <irfandurmus@gmail.com>
2012-03-23 01:45:57 +0200

    build.sh added

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
+#/bin/bash
 java -jar ../../tools/yuicompressor/build/yuicompressor-2.4.7.jar --type js -v src/rating.js -o min/rating.js
 java -jar ../../tools/yuicompressor/build/yuicompressor-2.4.7.jar --type css -v src/rating.css -o min/rating.css
