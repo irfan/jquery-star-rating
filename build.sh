7608ba4c6353efe01d85440c8042c27f0cd215c5
Irfan Durmus <irfandurmus@gmail.com>
2012-03-27 23:48:16 +0300

    extra space removed / comment added

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #/bin/bash
 
+# build.sh is a helper script for compiling/building javascript and css files
+# Please update these lines before use it
+
 YUI='../../tools/yuicompressor/build/yuicompressor-2.4.7.jar'
 FILES='src/*.js src/*.css'
 OUTPUT_DIR='min'
@@ -9,6 +12,6 @@ do
 	TYPE=${FILE/*./}
 	NAME=`basename "$FILE"`
 	OUTPUT="$OUTPUT_DIR/$NAME"
-	`java -jar $YUI  --type $TYPE -v $FILE -o $OUTPUT`
+	`java -jar $YUI --type $TYPE -v $FILE -o $OUTPUT`
 done
 
