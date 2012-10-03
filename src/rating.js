ec33ec1a112f9b98f68b9ae80b2bf56db4588cea
Roger Campos <roger@itnig.net>
2012-10-03 14:30:38 +0200

    Strip whitespaces

--- src/rating.js
+++ src/rating.js
@@ -1,5 +1,5 @@
 /* jQuery Star Rating Plugin
- * 
+ *
  * @Author
  * Copyright Nov 02 2010, Irfan Durmus - http://irfandurmus.com/
  *
@@ -16,12 +16,12 @@
 
 ;(function($){
     $.fn.rating = function(callback){
-        
+
         callback = callback || function(){};
 
         // each for all item
         this.each(function(i, v){
-            
+
             $(v).data('rating', {callback:callback})
                 .bind('init.rating', $.fn.rating.init)
                 .bind('set.rating', $.fn.rating.set)
@@ -29,7 +29,7 @@
                 .trigger('init.rating');
         });
     };
-    
+
     $.extend($.fn.rating, {
         init: function(e){
             var el = $(this),
@@ -38,68 +38,68 @@
                 childs = el.children(),
                 i = 0,
                 l = childs.length;
-            
+
             for (; i < l; i++) {
                 list = list + '<a class="star" title="' + $(childs[i]).val() + '" />';
                 if ($(childs[i]).is(':checked')) {
                     isChecked = $(childs[i]).val();
                 };
             };
-            
+
             childs.hide();
-            
+
             el
                 .append('<div class="stars">' + list + '</div>')
                 .trigger('set.rating', isChecked);
-            
-            $('a', el).bind('click', $.fn.rating.click);            
+
+            $('a', el).bind('click', $.fn.rating.click);
             el.trigger('hover.rating');
         },
         set: function(e, val) {
             var el = $(this),
                 item = $('a', el),
                 input = undefined;
-            
+
             if (val) {
                 item.removeClass('fullStar');
-                
+
                 input = item.filter(function(i){
                     if ($(this).attr('title') == val)
                         return $(this);
                     else
                         return false;
                 });
-                
+
                 input
                     .addClass('fullStar')
                     .prevAll()
                     .addClass('fullStar');
             }
-            
+
             return;
         },
         hover: function(e){
             var el = $(this),
                 stars = $('a', el);
-            
+
             stars.bind('mouseenter', function(e){
                 // add tmp class when mouse enter
                 $(this)
                     .addClass('tmp_fs')
                     .prevAll()
                     .addClass('tmp_fs');
-                
+
                 $(this).nextAll()
                     .addClass('tmp_es');
             });
-            
+
             stars.bind('mouseleave', function(e){
                 // remove all tmp class when mouse leave
                 $(this)
                     .removeClass('tmp_fs')
                     .prevAll()
                     .removeClass('tmp_fs');
-                
+
                 $(this).nextAll()
                     .removeClass('tmp_es');
             });
@@ -110,20 +110,20 @@
                 container = el.parent().parent(),
                 inputs = container.children('input'),
                 rate = el.attr('title');
-                
+
             matchInput = inputs.filter(function(i){
                 if ($(this).val() == rate)
                     return true;
                 else
                     return false;
             });
-            
+
             matchInput.attr('checked', true);
-            
+
             container
                 .trigger('set.rating', matchInput.val())
                 .data('rating').callback(rate, e);
         }
     });
-    
+
 })(jQuery);
